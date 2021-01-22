fdf019b36842e31ce12c028890e7738ed9ed088c
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-21 21:17:51 -0300

    Adicionar reducers

---
+++ AMPORAL/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  token: '',
+  name: '',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return {...state, name: action.payload.name};
+  }
+
+  return state;
+};
