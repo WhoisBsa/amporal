9f1dc5164e34494ffbb94cd4537d3344323c2e12
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-21 21:50:32 -0300

    Adicionar cores padroes ao projeto

--- AMPORAL/src/screens/Login/styled.js
+++ AMPORAL/src/screens/Login/styled.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components/native';
+import {PRIMARY} from '../../styles/colors';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
-  background-color: dodgerblue;
+  background-color: ${PRIMARY};
   justify-content: center;
   align-items: center;
 `;
---
+++ AMPORAL/src/styles/colors.js
@@ -0,0 +1,5 @@
+export const PRIMARY = '#1E90FF';
+export const SECONDARY = '#34F692';
+export const DARK = '#011627';
+export const LIGHT = '#EEF8FF';
+export const RED = '#E71D36';
