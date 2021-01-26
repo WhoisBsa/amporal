b9f817b431c0342619a2cef2ea1619b9784ba00a
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-26 20:18:59 -0300

    Adicionar cor a StatusBar

--- AMPORAL/App.js
+++ AMPORAL/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {store, persistor} from './src/store';
+import {StatusBar} from 'react-native';
 
 import MainStack from './src/navigations/MainStack';
 
-export default () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <MainStack />
-    </PersistGate>
-  </Provider>
-);
+export default () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar backgroundColor="dodgerblue" />
+        <MainStack />
+      </PersistGate>
+    </Provider>
+  );
+};
