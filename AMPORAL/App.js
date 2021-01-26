1982201fc62cfe5501067104891f76f98d200ac6
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-26 20:45:08 -0300

    Alterar cor da NavBar

--- AMPORAL/App.js
+++ AMPORAL/App.js
@@ -3,14 +3,17 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {store, persistor} from './src/store';
 import {StatusBar} from 'react-native';
+import NavigationBar from 'react-native-navbar-color';
 
 import MainStack from './src/navigations/MainStack';
 
 export default () => {
+  NavigationBar.setColor('#1E90FF');
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <StatusBar backgroundColor="dodgerblue" />
+        <StatusBar backgroundColor="dodgerblue" barStyle="light-content" />
         <MainStack />
       </PersistGate>
     </Provider>
