fceddc2c2b90e2edb9a6724daad3416360c1f48a
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-26 21:55:52 -0300

    Adicionar inputs, header e button

--- AMPORAL/src/screens/Login/index.js
+++ AMPORAL/src/screens/Login/index.js
@@ -1,6 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Container} from './styled';
+import {
+  Container,
+  LoginArea,
+  Input,
+  ActionButtonArea,
+  ActionButtonText,
+  Label,
+} from './styled';
 import Lottie from 'lottie-react-native';
 import {Platform} from 'react-native';
 
@@ -9,13 +16,26 @@ import mathTeatcher from '../../assets/mathTeacher.json';
 const Page = () => {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : null}>
+      <Label size="28">Bem-vindo(a) ao AMPORAL!</Label>
       <Lottie
-        style={{width: '80%'}}
+        style={{width: '80%', marginBottom: -30}}
         resizeMode="contain"
         source={mathTeatcher}
         autoPlay
-        loop
+        loop={false}
       />
+
+      <LoginArea>
+        <Label>Email</Label>
+        <Input placeholder="Email" keyboardType="email-address" />
+
+        <Label>Senha</Label>
+        <Input placeholder="Senha" secureTextEntry={true} />
+
+        <ActionButtonArea underlayColor="#4FF8A0">
+          <ActionButtonText>Entar</ActionButtonText>
+        </ActionButtonArea>
+      </LoginArea>
     </Container>
   );
 };
