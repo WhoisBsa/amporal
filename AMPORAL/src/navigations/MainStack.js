218b8d6954ea51c4f4912796ec55a2ef4f6c8c1f
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-27 21:52:16 -0300

    Alterar bg

--- AMPORAL/src/navigations/MainStack.js
+++ AMPORAL/src/navigations/MainStack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import Preload from '../screens/Preload';
 import Login from '../screens/Login';
+import Cadastro from '../screens/Cadastro';
 // import HomeStack from './HomeStack';
 
 export default createAppContainer(
@@ -10,12 +11,14 @@ export default createAppContainer(
     {
       Preload,
       Login,
+      Cadastro,
       // HomeStack,
     },
     {
       initialRouteName: 'Preload',
       defaultNavigationOptions: {
         headerShown: false,
+        cardStyle: {backgroundColor: 'dodgerblue'},
       },
     },
   ),
