3bfbed6afbae2c557e97952da970b9303f5d2923
WhoisBsa <mbscbsjbs@gmail.com>
2021-01-27 21:53:13 -0300

    Adicionar tela de cadastro

---
+++ AMPORAL/src/screens/Cadastro/styled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components/native';
+import {DARK, LIGHT, PRIMARY, SECONDARY} from '../../styles/colors';
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  background-color: ${PRIMARY};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginArea = styled.View`
+  width: 80%;
+`;
+
+export const Label = styled.Text`
+  font-size: ${(props) => props.size || '16'}px;
+  color: ${LIGHT};
+  font-weight: bold;
+  margin: 20px 0px 10px 0px;
+`;
+
+export const Input = styled.TextInput`
+  background-color: ${LIGHT};
+  border-radius: 10px;
+  height: 45px;
+  width: 100%;
+  font-size: 16px;
+  color: ${DARK};
+  font-weight: bold;
+  padding-left: 10px;
+`;
+
+export const ActionButtonArea = styled.TouchableHighlight`
+  background-color: ${SECONDARY};
+  border-radius: 10px;
+  height: 45px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+export const ActionButtonText = styled.Text`
+  font-size: 16px;
+  color: ${DARK};
+  font-weight: bold;
+`;
