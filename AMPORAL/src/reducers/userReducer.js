852aaae64c2a6c079f70d34ab77817cc051ec53b
WhoisBsa <mbscbsjbs@gmail.com>
2021-02-17 19:53:22 -0300

    Adicionar set_pass

--- AMPORAL/src/reducers/userReducer.js
+++ AMPORAL/src/reducers/userReducer.js
@@ -1,14 +1,15 @@
 const initialState = {
-  token: 'teste',
-  name: 'matheus',
+  token: '',
+  name: '',
+  password: '',
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'SET_NAME':
       return {...state, name: action.payload.name};
-    case 'SET_AGE':
-      return {...state, age: action.payload.age};
+    case 'SET_PASS':
+      return {...state, password: action.payload.password};
     case 'SET_TOKEN':
       return {...state, token: action.payload.token};
     default:
