cb83a555fd8e87684744c3b5b14f3d95a52b7e50
matheus <mbscbsjbs@gmail.com>
2021-04-22 15:41:31 -0300

    Adicionar player de video e variavel de ambiente

--- AMPORAL/src/screens/Aulas/index.js
+++ AMPORAL/src/screens/Aulas/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import Lottie from 'lottie-react-native';
+import YouTube from 'react-native-youtube';
 
 import { Container, Header, StartButton, StartText } from './styled';
 import { LIGHT } from '../../styles/colors';
+import { API_KEY } from '@env';
 
 const Page = (props) => {
+  console.log(props);
   return (
     <Container>
-      <Header>AULA - XX</Header>
+      <Header>{props.aula_titulo}</Header>
+
+      <YouTube
+        apiKey={API_KEY}
+        videoId={props.aula_link} // The YouTube video ID
+        loop // control whether the video should loop when ended
+        style={{ alignSelf: 'stretch', height: 300 }}
+      />
     </Container>
   );
 };
@@ -54,11 +63,21 @@ Page.navigationOptions = ({ navigation }) => {
 };
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    aula_id: state.aulaReducer.id,
+    aula_link: state.aulaReducer.link,
+    aula_material: state.aulaReducer.material,
+    aula_titulo: state.aulaReducer.titulo,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    setId: (id) => dispatch({ type: 'SET_ID', payload: { id } }),
+    setLink: (link) => dispatch({ type: 'SET_LINK', payload: { link } }),
+    setMaterial: (material) => dispatch({ type: 'SET_MATERIAL', payload: { material } }),
+    setTitulo: (titulo) => dispatch({ type: 'SET_TITULO', payload: { titulo } }),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Page);
