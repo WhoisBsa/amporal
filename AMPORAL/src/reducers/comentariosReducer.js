9cf412502fb0ec2b771f075460358cabf8be8113
matheus <mbscbsjbs@gmail.com>
2021-04-23 18:22:15 -0300

    Remover argumentos do payload

--- AMPORAL/src/reducers/comentariosReducer.js
+++ AMPORAL/src/reducers/comentariosReducer.js
@@ -3,7 +3,7 @@ const initialState = [];
 export default todo = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_COMENTARIO':
-            return { ...state, comentarios: [action.payload.comentarios[0].comentarios[0].comentario] };
+            return { ...state, comentarios: [action.payload] };
         default: return state;
     }
 }
